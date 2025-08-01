a9001f7c82b0076fc75c001760fde4966fae3ee3
ngandt184163 <ngan.dt184163@sis.hust.edu.vn>
2025-08-01 15:21:16 +0700

    hihi

---
+++ sd20303/bai6/baitap/h1.js
@@ -0,0 +1,8 @@
+let h1 = document.getElementById("text");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  h1.setAttribute("style", "background-color: red");
+});
+
+// PH64456
