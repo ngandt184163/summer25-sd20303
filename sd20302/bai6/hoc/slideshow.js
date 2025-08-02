2f40d4e9441b6b68b395eedba2dd9840b7493663
ngandt184163 <ngan.dt184163@sis.hust.edu.vn>
2025-08-02 17:33:22 +0700

    ca 5 2/8/25

---
+++ sd20302/bai6/hoc/slideshow.js
@@ -0,0 +1,54 @@
+let arr = [
+  "../img/anh1.jfif", // index 0
+  "../img/anh2.jfif", // index 1
+  "../img/anh3.jfif", // index 2
+  "../img/anh4.jfif", // index 3
+  "../img/anh5.jfif", // index 4
+];
+
+let index = 0; // chi so index cua mang bat dau tu 0
+let len = arr.length; // so luong anh trong mang: len = 5
+let img = document.getElementById("img"); // lay the img trong html
+img.src = arr[index]; // gan anh dau tien vao the img
+// img.setAttribute("src", arr[index]); // gan thuoc tinh src cho the img
+
+function chaytudong() {
+  return setInterval(function () {
+    if (index < len - 1) {
+      index++; // tang index len 1
+      img.src = arr[index]; // gan anh tiep theo vao the img
+    } else {
+      index = 0; // neu index bang len - 1 thi gan lai index = 0
+      img.src = arr[index]; // gan anh dau tien vao the img
+    }
+  }, 3000); // thuc hien ham moi 3 giay
+}
+
+let id_interval = chaytudong(); // bat dau chay tu dong
+
+// nut next
+let next = document.getElementById("next");
+next.onclick = function () {
+  clearInterval(id_interval); // dung chay tu dong
+  if (index < len - 1) {
+    index++; // tang index len 1
+    img.src = arr[index]; // gan anh tiep theo vao the img
+  } else {
+    index = 0; // neu index bang len - 1 thi gan lai index = 0
+    img.src = arr[index]; // gan anh dau tien vao the img
+  }
+  id_interval = chaytudong(); // bat dau chay tu dong lai
+};
+
+let pre = document.getElementById("pre");
+pre.onclick = function () {
+  clearInterval(id_interval); // dung chay tu dong
+  if (index > 0) {
+    index--; // giam index xuong 1
+    img.src = arr[index]; // gan anh truoc do vao the img
+  } else {
+    index = len - 1; // neu index bang 0 thi gan lai index = len - 1
+    img.src = arr[index]; // gan anh cuoi cung vao the img
+  }
+  id_interval = chaytudong(); // bat dau chay tu dong lai
+};
