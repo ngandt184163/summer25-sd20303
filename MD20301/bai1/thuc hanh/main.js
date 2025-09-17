d4b32b5e73a92b0aa36056e8d651840448e80ce8
ngandt184163 <ngan.dt184163@sis.hust.edu.vn>
2025-09-17 22:58:03 +0700

    17/9/25

---
+++ MD20301/bai1/thuc hanh/main.js
@@ -0,0 +1,12 @@
+console.log("day la file main.js");
+console.log("hahahhahaha");
+
+let a = 5;
+let A = 10;
+// console.log(a);
+console.log(A); // in ra gia tri cua bien A
+
+/**
+ * chu thich: co 2 loai, chu thich tren 1 dong
+ * va chu thich tren nhieu dong
+ */
---
+++ MD20302/bai1/thuchanh/main.js
@@ -0,0 +1,19 @@
+console.log("day la file main.js");
+
+/**
+ * chu thich trong js co 2 cach
+ * 1. cach 1: dung // de chu thich tren 1 dong
+ * 2. dung /* de chu thich tren nhieu dong
+ *
+ */
+
+let a = 5; // bien a co gia tri la 5
+let A = 10;
+console.log(a);
+console.log(A);
+
+/**
+ * chu thich tren nhieu dong
+ * day la chu thich tren nhieu dong
+ *
+ */
