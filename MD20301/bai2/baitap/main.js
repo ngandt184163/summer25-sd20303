b3973151ec00fdc5ee4de9052b4d896f0b182918
ngandt184163 <ngan.dt184163@sis.hust.edu.vn>
2025-09-20 13:58:52 +0700

    ca 3 20/9

---
+++ MD20301/bai2/baitap/main.js
@@ -0,0 +1,10 @@
+let num = 10;
+console.log(typeof num);
+let stringvar = "10";
+console.log(typeof stringvar); //
+let booleanvar = true;
+console.log(typeof booleanvar); // boolean
+let und;
+console.log(typeof und);
+let n = null;
+console.log(typeof n);
--- MD20301/bai2/thuchanh/main.js
+++ MD20301/bai2/thuchanh/main.js
@@ -1,3 +1,4 @@
+var bien3;
 var a = 5;
 let b = "hello";
 const c = true;
@@ -67,3 +68,66 @@ console.log(hoten.length);
 hoten = hoten.trim();
 console.log(hoten.length);
 console.log(hoten);
+
+// 5.2. kieu du lieu number
+let number1 = 5;
+let number2 = 3.14;
+console.log(number1, number2);
+console.log(typeof number1, typeof number2);
+let str5 = "xin chao"; // string
+
+console.log(Number.isInteger(number1));
+console.log(Number.isInteger(str5));
+
+console.log(isNaN(number1)); // false
+console.log(isNaN(number2)); // false
+console.log(isNaN(str5)); // true
+console.log(typeof str5); // true
+
+// console.log(isNaN("hello")); // true
+// if (isNaN(str5) === false) {
+//   console.log("hehe");
+// }
+
+// if (isNaN(number1) === false) {
+//   console.log("hehe");
+// } else {
+//   console.log("haha");
+// }
+
+// 5.3 boolean
+let dung = true;
+let sai = false;
+console.log(dung, sai);
+console.log(typeof dung, typeof sai);
+
+// undefined va null
+// 1. undefined: la kieu du lieu duoc js tu dong
+// gan khi khai bao bien ma khong gan gia tri
+// hoac chung ta gan gia tri la undefined
+// hoac la mot thuoc tinh khong ton tai cua doi tuong
+// hoac mot phan tu khong ton tai cua mang
+let number3;
+console.log(number3); // undefined
+console.log(typeof number3); // undefined
+// 2. null: la kieu du lieu do ltv tu gan
+// voi muc dich rieng, thuong la bieu thi
+// mot gia tri rong ( hoac khong co gia tri)
+let number4 = null;
+console.log(number4); // null
+console.log(typeof number4); // object
+
+// 6. mang
+// 6.1: khai bao mang
+let arr1 = ["xin chao", 6, true, null, undefined];
+console.log(arr1);
+let arr2 = new Array("xin chao", 6, true, null, undefined);
+console.log(arr2);
+// 6.2: truy cap phan tu trong mang
+console.log(arr1[0]); // lay ra phan tu mang "xin chao"
+console.log(arr1[2]); // lay ra phan tu mang true
+arr1[0] = "xin chao cac ban"; // cap nhat lai gia tri phan tu mang
+console.log(arr1[0]);
+
+let a = 6;
+a = "gan gia tri moi";
