c0a438bf04dbff5d9d3673fa92b6b9deddc01da6
ngandt184163 <ngan.dt184163@sis.hust.edu.vn>
2026-03-31 18:36:27 +0700

    assignment gd 2

--- Vuejs/ca5/front-end-framework/src/baihoc/buoi10_App.vue
+++ Vuejs/ca5/front-end-framework/src/baihoc/buoi10_App.vue
@@ -1,3 +1,45 @@
+<script setup>
+/**
+ * 1. Lifecycle: la vong doi cua
+ * component, la tat cac cac qua trinh 
+ * tu luc no duoc sinh ra den khi no
+ * bi huy di
+ * - vong doi cua component la tu luc
+ * no duoc them vao DOM den khi no duoc
+ * xoa khoi DOM
+ * 2. Lifecycle Hooks là gì?
+  Hooks = các “điểm móc” để chạy code theo từng giai đoạn  
+  - giai doan o day la:
+  + truoc khi them vao DOM
+  + sau khi them vao DOM
+  + truoc khi xoa khoi DOM
+  + sau khi xoa khoi DOM  
+  + truoc khi cap nhat DOM
+  + sau khi cap nhat DOM
+  3. cac hook quan trong
+  3.1. onMounted()
+  - chay khi component duoc hien thi
+  len man hinh
+  - sd khi
+  + lay du lieu
+  + goi API
+  + thao tac voi DOM
+  3.2. onUnMounted()
+  - chay khi component bi xoa khoi DOM
+  - sd khi
+  + don dep du lieu, dong ket noi toi server
+  + don dep su kien,...
+  3.4 onBeforeMount()
+    Chạy trước khi render
+  3.5 onBeforeUnmount()
+    Chạy trước khi bị hủy
+  3.2 onUpdated()
+    Khi data thay đổi → UI update
+  3.5 onBeforeUpdate()
+    Chạy trước khi Update
+ */
+</script>
+
 <template>
   <nav>
     <router-link to="/">Home</router-link>
--- Vuejs/ca5/front-end-framework/src/views/AboutView.vue
+++ Vuejs/ca5/front-end-framework/src/views/AboutView.vue
@@ -1,3 +1,43 @@
+<script setup>
+import {
+  ref,
+  onMounted,
+  onUnmounted,
+  onBeforeMount,
+  onBeforeUnmount,
+  onUpdated,
+  onBeforeUpdate,
+} from "vue";
+
+onMounted(() => {
+  console.log("on Mounted");
+});
+
+onUnmounted(() => {
+  console.log("on UnMounted");
+});
+
+onBeforeMount(() => {
+  console.log("on Before Mount");
+});
+
+onBeforeUnmount(() => {
+  console.log("on Before UnMount");
+});
+
+const count = ref(0);
+
+onUpdated(() => {
+  console.log("on Updated:", document.getElementById("count").innerText);
+});
+onBeforeUpdate(() => {
+  console.log(
+    "on Before Updated: ",
+    document.getElementById("count").innerText,
+  );
+});
+</script>
 <template>
   <h1>About Page</h1>
+  <button id="count" @click="count++">{{ count }}</button>
 </template>
